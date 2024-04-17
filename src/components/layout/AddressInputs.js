edc79763ac19e110f7cd00a933b13122c7695529
manuel-gonzales <141975981+manuel-gonzales@users.noreply.github.com>
2024-04-17 15:47:34 -0500

    added warning message for incorrect phone inputs

--- src/components/layout/AddressInputs.js
+++ src/components/layout/AddressInputs.js
@@ -2,15 +2,40 @@ export default function AddressInputs({addressProps, setAddressProp, disabled=fa
     const {phone, streetAddress, city} = addressProps;
     return (
         <>
-        
             <label>
                 Teléfono
             </label>
-            <input
-                disabled={disabled}
-                type="tel" placeholder= "Telefono"
-                value={phone || ''} onChange={ev => setAddressProp('phone', ev.target.value)}
-            />
+            <div>
+                <input
+                    disabled={disabled}
+                    type="tel" 
+                    placeholder="Telefono"
+                    value={phone || ''}
+                    onChange={ev => {
+                        const input = ev.target.value;
+                        const regex = /^[0-9]*$/; // Expresión regular para permitir solo números
+                        if (input.length <= 9 && regex.test(input)) {
+                            setAddressProp('phone', input);
+                            if (input.length !== 9) {
+                                document.getElementById('error-message').innerText = "El teléfono debe tener 9 dígitos";
+                            } else {
+                                document.getElementById('error-message').innerText = '';
+                            }
+                        } else {
+                            // Muestra un mensaje de error debajo del campo de entrada
+                            if (input.length > 9) {
+                                document.getElementById('error-message').innerText = "El teléfono debe tener 9 dígitos";
+                            } else {
+                                document.getElementById('error-message').innerText = "¡Solo se permiten números!";
+                            }
+                            setTimeout(() => {
+                                document.getElementById('error-message').innerText = ''; // Borra el mensaje 
+                            }, 3000); //(3 segundos)
+                        }
+                    }}
+                />
+                <div id="error-message" style={{ color: 'red' }}></div>
+            </div>
             <label>
                 Dirección
             </label>
@@ -24,7 +49,7 @@ export default function AddressInputs({addressProps, setAddressProp, disabled=fa
             </label>
             <input
                 disabled={disabled}
-                type="text" placeholder= "Ciudad"
+                type="text" placeholder= "Referencia"
                 value={city || ''} onChange={ev => setAddressProp('city', ev.target.value)}
             />
         </>
